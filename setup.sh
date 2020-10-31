0d90d8ca11132df046bc0e55f7856cc088342972
Vladyslav Prokopenko <v_p164@zeus.cs.txstate.edu>
2020-10-31 01:23:29 -0500

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+wget https://github.com/v-prokopenko/sql-plus-readline/raw/master/rlwrap.tar.gz
+tar xzf rlwrap.tar.gz
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib
+export PATH=$PATH:$HOME/bin
